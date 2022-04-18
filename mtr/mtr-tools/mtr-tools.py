6b23db89a8bd45f88c8ed18a309a4b8c22263108
aliuzzz <roy6152@163.com>
2022-04-18 13:20:53 +0800

    chromedriver  版本100.0.4896.127

--- mtr/mtr-tools/mtr-tools.py
+++ mtr/mtr-tools/mtr-tools.py
@@ -1,13 +1,15 @@
 import sys
 import ctypes
-from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget
+
+from PyQt6 import QtWidgets, QtGui, QtCore
+#from PyQt6.QtWidgets import QMainWindow, QApplication
 from Ui_mtrtools import Ui_MainWindow  #导入你写的界面类
  
 class MyMainWindow(QMainWindow,Ui_MainWindow): #这里也要记得改
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
-
+'''
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
@@ -15,3 +17,13 @@ if __name__ == "__main__":
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)  #设置任务栏图标
     myWin.show()
     sys.exit(app.exec_())    
+'''
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = MyMainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
